09c13e71bb8c9f481508c55345b22b23f0fb98c8
Ayaka.Y <ayaka.yamanaka.8@gmail.com>
2017-09-08 18:01:40 +0900

    I placed an object in the room

--- Assets/AyakaScript/Player/PlayerController.cs
+++ Assets/AyakaScript/Player/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     // 速度
     public Vector2 SPEED = new Vector2(0.01f, 0.01f);
@@ -26,6 +27,9 @@ public class PlayerController : MonoBehaviour {
     //Animator
     Animator anim;
 
+    //衝突しているか否か
+    public bool collisionFlug = false;
+    string tagName = null;
 
     // Use this for initialization
     void Start()
@@ -61,41 +65,57 @@ public class PlayerController : MonoBehaviour {
         {
             ////移動開始
             //click = true;
-
-            //クリックした位置を目標位置に設定
-            targetPos = Input.mousePosition;
-
-            //ワールド座標に変換
-            worldMousePos = Camera.main.ScreenToWorldPoint(targetPos);
-            worldMousePos.z = 10f;
-            
-            //マウスの座標と現在の座標から、どの方向に動いているか確認
-            //Left
-            if(worldMousePos.x < transform.position.x)
+            if (!collisionFlug)
             {
-                //移動開始
-                left = true;
+                //クリックした位置を目標位置に設定
+                targetPos = Input.mousePosition;
+
+                //ワールド座標に変換
+                worldMousePos = Camera.main.ScreenToWorldPoint(targetPos);
+                worldMousePos.z = 10f;
+
+                //マウスの座標と現在の座標から、どの方向に動いているか確認
+                //Left
+                if (worldMousePos.x < transform.position.x)
+                {
+                    //移動開始
+                    left = true;
+                }
+                //Right
+                else
+                {
+                    //移動開始
+                    click = true;
+                }
+
+                //動く
+                iTween.MoveTo(this.gameObject, iTween.Hash(
+                    "position", worldMousePos,
+                    "time", 0.5f,
+                    "oncomplete", "OnCompleteCallback",
+                    "oncompletetarget", this.gameObject,
+                    "easeType", "linear"));
             }
-            //Right
             else
             {
-                //移動開始
-                click = true;
+                //クリックして、オブジェクトがあったら
+                GameObject obj = getClickObject();
+                if(obj != null)
+                {
+                    //タグが、衝突判定と同じタグだったら
+                    if (obj.tag == tagName)
+                    {
+                        Debug.Log("取得");
+                        tagName = null;
+                    }
+                }
             }
-
-            //動く
-            iTween.MoveTo(this.gameObject, iTween.Hash(
-                "position", worldMousePos,
-                "time", 0.5f,
-                "oncomplete", "OnCompleteCallback",
-                "oncompletetarget", this.gameObject,
-                "easeType", "linear"));
         }
 
-            //if(this.transform.position == worldMousePos)
-            //{
-            //    click = false;
-            //}
+        //if(this.transform.position == worldMousePos)
+        //{
+        //    click = false;
+        //}
 
         //壁処理（Mashf.Clamp(制限する座標値, 最小値, 最大値)
         this.transform.position = (new Vector3(Mathf.Clamp(this.transform.position.x, wall_Left, wall_Right),
@@ -111,6 +131,57 @@ public class PlayerController : MonoBehaviour {
         //移動フラグをfalse
         click = false;
         left = false;
+    }
+
+
+    //---------------------------------------------//   
+    //  衝突判定
+    //---------------------------------------------//   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //tagName = collision.gameObject.tag;
+        //Vector2 position = new Vector2(0, 0);
+        //position = collision.contacts[0].point;
+        //Debug.Log(position);
+        //collisionFlug = true;
+        //position = new Vector2(position.x, position.y);
+        //iTween.MoveTo(this.gameObject, iTween.Hash(
+        //    "position", position,
+        //    "time", 0.01f,
+        //    "oncomplete", "OnCompleteCallback",
+        //    "oncompletetarget", this.gameObject,
+        //    "easeType", "linear"));
+        //Debug.Log("衝突");
+    }
+
+
+    //衝突、動くのが終わった後に呼ばれる関数
+    void OnCollision()
+    {
+        //デバック用
+        //Debug.Log("animatingStop");
+        //移動フラグをfalse
+        click = false;
+        left = false;
+
+    }
+
+    // 左クリックしたオブジェクトを取得する関数(2D)
+    private GameObject getClickObject()
+    {
+        GameObject result = null;
+        // 左クリックされた場所のオブジェクトを取得
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+            if (collition2d)
+            {
+                result = collition2d.transform.gameObject;
+            }
+        }
+        return result;
     }
 
 }
