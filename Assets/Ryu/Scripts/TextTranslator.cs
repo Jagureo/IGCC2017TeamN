076b9858994d55b1592c3d5f7a5763ea98541ace
RyutaroSUZUKI <szkrtr1992@yahoo.co.jp>
2017-09-08 14:54:04 +0900

    Add simple Localization System
    - It reads key and translated text pair from TextAsset(xml file).

---
+++ Assets/Ryu/Scripts/TextTranslator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent
+,RequireComponent(typeof(Text))
+]
+public class TextTranslator : MonoBehaviour {
+  private Text target;
+
+  [SerializeField]
+  private string key;
+
+  public string Key {
+    get { return key; }
+    set {
+      key = value;
+      Translate();
+    }
+  }
+
+  private void Awake() {
+    target = GetComponent<Text>();
+  }
+
+  private void Start() {
+    LocalizationManager.Instance.onLanguageChanged += Translate;
+    if (key.Length > 0) {
+      Translate();
+    }
+  }
+
+  private void OnDestroy() {
+    LocalizationManager.Instance.onLanguageChanged -= Translate;
+  }
+
+  private void Translate() {
+    target.text = LocalizationManager.Instance.GetText(key);
+  }
+
+}
