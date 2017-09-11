6bce3b5af3a3ae44c14ac791d82c0d33d85c13d1
Ayaka.Y <ayaka.yamanaka.8@gmail.com>
2017-09-11 12:40:31 +0900

    Item acquisition callback

--- Assets/AyakaScript/Player/PlayerController.cs
+++ Assets/AyakaScript/Player/PlayerController.cs
@@ -3,6 +3,33 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    //取得した際の画像変更用
+    SpriteRenderer BedSpriteRenderer;
+    SpriteRenderer SidetableSpriteRenderer;
+    SpriteRenderer DeskSpriteRenderer;
+    SpriteRenderer ChestSpriteRenderer;
+    SpriteRenderer ToolboxSpriteRenderer;
+
+
+    public Sprite BedPut;
+    public Sprite SidetablePut;
+    public Sprite DeskPut;
+    public Sprite ChestPut;
+    public Sprite Chest2Put;
+    public Sprite ToolboxPut;
+    public Sprite Toolbox2Put;
+
+    GameObject[] beds;
+    GameObject[] sidetables;
+    GameObject[] desks;
+    GameObject[] chests;
+    GameObject[] toolboxs;
+
+    bool chestGet = false;
+    bool toolboxGet = false;
+    int toolboxTime = 0;
+    int chestTime = 0;
+
 
     // 速度
     public Vector2 SPEED = new Vector2(0.01f, 0.01f);
@@ -31,9 +58,27 @@ public class PlayerController : MonoBehaviour
     public bool collisionFlug = false;
     string tagName = null;
 
+
     // Use this for initialization
     void Start()
     {
+        //------------------------------------------------------------------------//
+        // 取得した際の画像変更のために、それぞれのオブジェクトを取得しておく     //
+        //------------------------------------------------------------------------//
+        
+        beds = GameObject.FindGameObjectsWithTag("Bed");
+        sidetables = GameObject.FindGameObjectsWithTag("sidetable");
+        desks = GameObject.FindGameObjectsWithTag("desk");
+        chests = GameObject.FindGameObjectsWithTag("chest");
+        toolboxs = GameObject.FindGameObjectsWithTag("toolbox");
+
+        // このobjectのSpriteRendererを取得
+        BedSpriteRenderer = beds[0].GetComponent<SpriteRenderer>();
+        SidetableSpriteRenderer = sidetables[0].GetComponent<SpriteRenderer>();
+        DeskSpriteRenderer = desks[0].GetComponent<SpriteRenderer>();
+        ChestSpriteRenderer = chests[0].GetComponent<SpriteRenderer>();
+        ToolboxSpriteRenderer = toolboxs[0].GetComponent<SpriteRenderer>();
+
         //Animatorをキャッシュ
         anim = GetComponent<Animator>();
         //フラグはfalse
@@ -63,10 +108,10 @@ public class PlayerController : MonoBehaviour
         //左クリックしたら、そっちの方向に移動 
         if (Input.GetMouseButton(0))
         {
-            ////移動開始
-            //click = true;
             if (!collisionFlug)
             {
+                ////移動開始
+                //click = true;
                 //クリックした位置を目標位置に設定
                 targetPos = Input.mousePosition;
 
@@ -96,26 +141,38 @@ public class PlayerController : MonoBehaviour
                     "oncompletetarget", this.gameObject,
                     "easeType", "linear"));
             }
-            else
-            {
-                //クリックして、オブジェクトがあったら
-                GameObject obj = getClickObject();
-                if(obj != null)
-                {
-                    //タグが、衝突判定と同じタグだったら
-                    if (obj.tag == tagName)
-                    {
-                        Debug.Log("取得");
-                        tagName = null;
-                    }
-                }
-            }
         }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    //クリックして、オブジェクトがあったら
+        //    GameObject obj = getClickObject();
+        //    if (obj != null)
+        //    {
+        //        //タグが、衝突判定と同じタグだったら
+        //        if (obj.tag == tagName)
+        //        {
+        //            Debug.Log("取得");
+        //            tagName = null;
+        //        }
+        //    }
+
+        //}
 
         //if(this.transform.position == worldMousePos)
         //{
         //    click = false;
         //}
+
+        //棚のアニメーション
+        if(chestGet)
+        {
+            ChestChangeSprite();
+        }
+        //ツールボックスのアニメーション
+        if(toolboxGet)
+        {
+            ToolboxChangeSprite();
+        }
 
         //壁処理（Mashf.Clamp(制限する座標値, 最小値, 最大値)
         this.transform.position = (new Vector3(Mathf.Clamp(this.transform.position.x, wall_Left, wall_Right),
@@ -138,21 +195,64 @@ public class PlayerController : MonoBehaviour
     //  衝突判定
     //---------------------------------------------//   
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        //tagName = collision.gameObject.tag;
-        //Vector2 position = new Vector2(0, 0);
-        //position = collision.contacts[0].point;
-        //Debug.Log(position);
-        //collisionFlug = true;
-        //position = new Vector2(position.x, position.y);
-        //iTween.MoveTo(this.gameObject, iTween.Hash(
-        //    "position", position,
-        //    "time", 0.01f,
-        //    "oncomplete", "OnCompleteCallback",
-        //    "oncompletetarget", this.gameObject,
-        //    "easeType", "linear"));
-        //Debug.Log("衝突");
+        collisionFlug = true;
+        tagName = collision.gameObject.tag;
+        if (Input.GetMouseButton(0))
+        {
+            //クリックして、オブジェクトがあったら
+            GameObject obj = getClickObject();
+            if (obj != null)
+            {
+                //タグが、衝突判定と同じタグだったら
+                if (obj.tag == tagName)
+                {
+                    Debug.Log("取得");
+                    switch(tagName)
+                    {
+                        case "Bed":
+                            BedSpriteRenderer.sprite = BedPut;
+                            break;
+                        case "sidetable":
+                            SidetableSpriteRenderer.sprite = SidetablePut;
+                            break;
+                        case "desk":
+                            DeskSpriteRenderer.sprite = DeskPut;
+                            break;
+                        case "chest":
+                            ChestSpriteRenderer.sprite = ChestPut;
+                            chestGet = true;
+                            break;
+                        case "toolbox":
+                            ToolboxSpriteRenderer.sprite = ToolboxPut;
+                            toolboxGet = true;
+                            break;
+                        
+
+                    }
+                    tagName = null;
+                }
+            }
+            else
+            {
+                collisionFlug = false;
+            }
+        }
+        else
+        {
+            Vector2 position = new Vector2(0, 0);
+            position = collision.contacts[0].point;
+            //Debug.Log(position);
+            collisionFlug = true;
+            position = new Vector2(position.x, position.y);
+            iTween.MoveTo(this.gameObject, iTween.Hash(
+                "position", position,
+                "time", 0.01f,
+                "oncomplete", "OnCompleteCallback",
+                "oncompletetarget", this.gameObject,
+                "easeType", "linear"));
+        }
     }
 
 
@@ -183,5 +283,24 @@ public class PlayerController : MonoBehaviour
         }
         return result;
     }
+
+    void ChestChangeSprite()
+    {
+        chestTime++;
+        if(chestTime >= 30)
+        {
+            ChestSpriteRenderer.sprite = Chest2Put;
+        }
+    }
+
+    void ToolboxChangeSprite()
+    {
+        toolboxTime++;
+        if (toolboxTime >= 30)
+        {
+            ToolboxSpriteRenderer.sprite = Toolbox2Put;
+        }
+    }
+
 
 }
