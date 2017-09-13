43bc66314184b6280eca801b43c439793016fb7c
RyutaroSUZUKI <szkrtr1992@yahoo.co.jp>
2017-09-13 10:48:42 +0900

    Add GenderSelectionPanel, create new Scene for test it
    - it is assumed to display before the player becomes operable.
    - press ok button to change settings. (the panel is deactivated when press ok.)

---
+++ Assets/Ryu/Scripts/GenderSelectionButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GenderSelectionButton : MonoBehaviour {
+  [SerializeField]
+  private Image targetImage;
+  [SerializeField]
+  private Sprite offSprite;
+  [SerializeField]
+  private Sprite onSprite;
+
+  public void ChangeImage(bool isOn) {
+    Sprite sprite;
+    if (isOn) {
+      sprite = onSprite;
+    }
+    else {
+      sprite = offSprite;
+    }
+    targetImage.sprite = sprite;
+  }
+}
