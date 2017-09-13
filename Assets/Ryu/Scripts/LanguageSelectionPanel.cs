a6c5bccf8dce5e2360604dbb0d1e563eb3f274f3
RyutaroSUZUKI <szkrtr1992@yahoo.co.jp>
2017-09-13 16:06:26 +0900

    Add Language Selection Panel
    - display it when pushed a small button on the Title Screen

---
+++ Assets/Ryu/Scripts/LanguageSelectionPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class LanguageSelectionPanel : MonoBehaviour {
+  public delegate void ValueChangeEvent(Toggle newActive);
+  public event ValueChangeEvent onValueChange;
+
+  private ToggleGroup toggleGroup;
+  private string selectedLanguage;
+
+  private void Awake() {
+    toggleGroup = GetComponent<ToggleGroup>();
+    selectedLanguage = toggleGroup.ActiveToggles().FirstOrDefault().name;
+  }
+
+  public void Start() {
+    foreach (Transform transformToggle in gameObject.transform) {
+      var toggle = transformToggle.GetComponent<Toggle>();
+      if (toggle == null) continue;
+      toggle.onValueChanged.AddListener((isSelected) => {
+        if (!isSelected) return;
+        var activeToggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        onValueChange(activeToggle);
+      });
+    }
+
+    onValueChange += (newActive) => {
+      selectedLanguage = newActive.name;
+    };
+  }
+
+  public void Submit() {
+    gameObject.SetActive(false);
+    LocalizationManager.Instance.ChangeLanguage(selectedLanguage);
+  }
+
+}
