db7141b831f561ff6ecaa78ce70705f66b7cd0e9
RyutaroSUZUKI <szkrtr1992@yahoo.co.jp>
2017-09-14 18:32:46 +0900

    Add Language Selection Panel Texture

---
+++ Assets/Ryu/Scripts/LanguageSelectionButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LanguageSelectionButton : MonoBehaviour {
+  private Image image;
+  [SerializeField]
+  private Sprite onSprite;
+  [SerializeField]
+  private Sprite offSprite;
+
+  private void Awake() {
+    image = GetComponent<Image>();
+  }
+
+  public void ChangeSprite(bool isOn) {
+    if (isOn) {
+      image.sprite = onSprite;
+    }
+    else {
+      image.sprite = offSprite;
+    }
+  }
+
+}
